f170e1e4336b055e757b477ccb5412956fbeb8e7
ingvijonasson <contact@ingvijonasson.com>
2021-04-12 15:57:57 +0200

    Update favicon and site title and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,18 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'sporkin-telja',
+    title: 'Spörkin telja',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'is',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Þekktu hreyfingar barnsins þíns'
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
